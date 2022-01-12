1387be673c7ac6874975b1d3fbe6b4f46574f6c0
zvrvdmtr <zverevdmitri.87@gmail.com>
2022-01-12 23:32:09 +0300

    Refactoring program.fs. Add parsing functions.

--- Parser.fs
+++ Parser.fs
@@ -1,28 +1,31 @@
-module Parser 
+module Parser
 
     open FSharp.Data
 
-    type SearchObject = {Musician: string; Album: string}
+    type Record = {Band: string; Album: string}
 
     let file = CsvFile.Load("/Users/zverev/Documents/fsharp/text.csv")
 
-    let objects = file.Rows |> Seq.map (fun i -> {Musician = i[1]; Album = i[2]})
+    let objects = file.Rows |> Seq.map (fun i -> {Band = i[1]; Album = i[2]})
 
     let buildObject (argv: string[]) = 
         match argv.Length with
-        | 1 -> {Musician = argv[0]; Album = ""}
-        | 2 -> {Musician = argv[0]; Album = argv[1]}
-        | _ -> {Musician = ""; Album = ""}
+        | 0 -> {Band = ""; Album = ""}
+        | 1 -> {Band = argv[0]; Album = ""}
+        | _ -> {Band = argv[0]; Album = argv[1]}
 
-    let IsAlbumEqual album = List.filter (fun x -> x.Album = album)
+    let IsAlbumEqual album =
+        List.filter (fun x -> x.Album = album)
 
-    let IsMusicianEqual musician = List.filter (fun x -> x.Musician = musician)
+    let IsBandEqual musician =
+        List.filter (fun x -> x.Band = musician)
 
-    let IsObjectsEqual completeObject = List.filter (fun x -> x = completeObject)
+    let IsObjectsEqual completeObject = 
+        List.filter (fun x -> x = completeObject)
 
     let search object = 
         match object with
-        | {Musician = ""; Album = ""} -> []
-        | {Musician = ""} -> IsAlbumEqual object.Album (Seq.toList objects)
-        | {Album = ""} -> IsMusicianEqual object.Musician (Seq.toList objects)
+        | {Band = ""; Album = ""} -> []
+        | {Band = ""} -> IsAlbumEqual object.Album (Seq.toList objects)
+        | {Album = ""} -> IsBandEqual object.Band (Seq.toList objects)
         | _ -> IsObjectsEqual object (Seq.toList objects)
--- Program.fs
+++ Program.fs
@@ -1,9 +1,37 @@
-﻿module Program =
+﻿// Add worker for file convertation from xlsx to csv using Gnumeric 
+// Flow: 1) Download file 2) Convert file 3) Save with name foo_cuurent_date.csv 4) Remove old file
+// 5) If file doesnt ready and user made request, send response "Sorry service anavailable."
+// Add config for csv file path for parsing
+// Add command line flags "all" - for all matching rows, "first" - only first matching row
+module Program =
 
     open Parser
+    open System
+
+    let queryByParams (line:string[]) = 
+        match line.Length with
+        | 0 -> printf "Not enought values"
+        | 1 | 2 -> 
+                line
+                |> buildObject
+                |> search
+                |> Seq.iter (printf "%A\n")
+        | _ -> printf "Too much values. Only 2 values require.\n"
+
+    let parseParams (input:string) = 
+        let parser (value:string) = value.Split [|','|] |> Array.map (fun (x:string) -> x.Trim())
+        
+        match input with
+        | "" -> [||]
+        | _ -> parser input
 
     [<EntryPoint>]
     let main argv = 
-        let obj = buildObject argv
-        obj |> search |> Seq.iter (printf "%A")
+        printf "Enter band and/or album.\nBand and album name must be comma separated.\n"
+
+        let rec readParams (input:string) = 
+            input |> parseParams |> queryByParams
+            readParams (Console.ReadLine())
+
+        readParams (Console.ReadLine())
         0
